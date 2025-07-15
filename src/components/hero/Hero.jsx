170a57d0e1bd18036ee078652ea0b99de19617a1
Nico Rithner <nicorithner@gmail.com>
2025-07-14 19:27:23 -0600

    Hero section mobile and desktop.

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,9 +1,6 @@
 import heroImage from "../../assets/tak-juni-2025.jpg";
-import {
-  ButtonHighlight,
-  ButtonBorder,
-  ButtonNoBorder,
-} from "../../ui/buttons";
+import heroImageMobile from "../../assets/lifters.png";
+import { ButtonHighlight, ButtonBorder } from "../../ui/buttons";
 import "./Hero.styles.scss";
 
 const Hero = () => {
@@ -12,15 +9,23 @@ const Hero = () => {
       <img
         src={heroImage}
         alt="Trondheim Atletklubb weightlifting gym interior"
-        className="hero__image"
+        className="hero__image hero__image--desktop"
+      />
+      <img
+        src={heroImageMobile}
+        alt="Trondheim Atletklubb weightlifters training"
+        className="hero__image hero__image--mobile"
       />
       <div className="hero__overlay"></div>
       <div className="hero__content">
-        <h1 className="hero__title">Join Trondheim's weightlifting community</h1>
-        <p className="hero__subtitle">learn and train Olympic Weightlifting</p>
+        <h1 className="hero__title">Finn Styrken Din Hos Oss</h1>
+        <p className="hero__subtitle">
+          Trygt, sosialt og lærerik vektløfting treningsmiljø for alle i
+          Trondheims sentrum.
+        </p>
         <div className="hero__buttons">
-          <ButtonHighlight>Join</ButtonHighlight>
-          <ButtonBorder>Learn More</ButtonBorder>
+          <ButtonHighlight>bli medlem</ButtonHighlight>
+          <ButtonBorder>learn more</ButtonBorder>
         </div>
       </div>
     </section>
