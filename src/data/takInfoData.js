8cd10a5979f3dc25ec3ecc1a583531a7a220be64
Nico Rithner <nicorithner@gmail.com>
2025-07-19 17:09:31 -0600

    Update email

--- src/data/takInfoData.js
+++ src/data/takInfoData.js
@@ -1,5 +1,5 @@
 export const takInfoData = {
-  epost: "info@takweightlifting.org",
+  epost: "taklubb.info@gmail.com",
   telefon: "+47 123 45 678",
   sted: "Mellomveien 5, 7067 Trondheim, Norway",
 };
