23f2d534947e9c2ac39db1b27a605ec7ebabb8da
Nico Rithner <nicorithner@gmail.com>
2025-07-20 09:44:19 -0600

    Refactor to use pricingData.js

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -1,5 +1,7 @@
 import { Section } from "../../ui/sections";
+import { ButtonHighlight } from "../../ui/buttons";
 import takCommunityImage from "../../assets/tak-community.WebP";
+import { membershipPricing } from "../../data/pricingData";
 import "./Pricing.styles.scss";
 
 const Pricing = () => {
@@ -14,32 +16,14 @@ const Pricing = () => {
       <div className="pricing__content">
         <div className="pricing__info">
           <div className="pricing__table">
-            <div className="pricing__row">
-              <span className="pricing__description">
-                Treningsavgift t.o.m. året man fyller 20
-                år/studenter/pensjonister:
-              </span>
-              <span className="pricing__price">2.400,- per år</span>
-            </div>
-
-            <div className="pricing__row">
-              <span className="pricing__description">
-                Treningsavgift f.o.m. året man fyller 21 år:
-              </span>
-              <span className="pricing__price">3.600,- per år</span>
-            </div>
-
-            <div className="pricing__row">
-              <span className="pricing__description">
-                Familie (samme husstand):
-              </span>
-              <span className="pricing__price">4.800,- per år</span>
-            </div>
-
-            <div className="pricing__row">
-              <span className="pricing__description">Medlemsavgift</span>
-              <span className="pricing__price">100,- per år</span>
-            </div>
+            {membershipPricing.map((membership) => (
+              <div key={membership.id} className="pricing__row">
+                <span className="pricing__description">
+                  {membership.description}
+                </span>
+                <span className="pricing__price">{membership.price}</span>
+              </div>
+            ))}
           </div>
 
           <div className="pricing__access-info">
@@ -48,6 +32,12 @@ const Pricing = () => {
               slik at våre medlemmer kan trene når mellom 16.00 og 23.00 på
               hverdager og 07.00 og 23.00 i helger.
             </p>
+          </div>
+
+          <div className="pricing__cta">
+            <ButtonHighlight href="https://example.com/membership-signup">
+              Bli Medlem
+            </ButtonHighlight>
           </div>
         </div>
 
