a883d3f4ac253bcee2713e24f9fb373acc76d3dd
Nico Rithner <nicorithner@gmail.com>
2025-07-20 09:44:04 -0600

    Create file for pricing data

---
+++ src/data/pricingData.js
@@ -0,0 +1,23 @@
+export const membershipPricing = [
+  {
+    id: 1,
+    description:
+      "Treningsavgift t.o.m. året man fyller 20 år/studenter/pensjonister:",
+    price: "2.400,- per år",
+  },
+  {
+    id: 2,
+    description: "Treningsavgift f.o.m. året man fyller 21 år:",
+    price: "3.600,- per år",
+  },
+  {
+    id: 3,
+    description: "Familie (samme husstand):",
+    price: "4.800,- per år",
+  },
+  {
+    id: 4,
+    description: "Medlemsavgift",
+    price: "100,- per år",
+  },
+];
