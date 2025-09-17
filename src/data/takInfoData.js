7bac321d21705c124a2ded99300a33bda99fbe6e
Nico Rithner <nicorithner@gmail.com>
2025-09-17 12:52:18 -0600

    Update email address

--- src/data/takInfoData.js
+++ src/data/takInfoData.js
@@ -1,5 +1,5 @@
 export const takInfoData = {
-  epost: "taklubb.info@gmail.com",
+  epost: "trondheim@atletklubben.no",
   // telefon: "",
   sted: "Mellomveien 5, 7067 Trondheim, Norway",
   facebook: "TAK Facebook Page",
