fbec448595c08fa0d538b752f32df3c31816b3df
Nico Rithner <nicorithner@gmail.com>
2025-10-26 07:35:36 -0600

    Update base

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tak-nettsiden/',
+  base: "/",
   css: {
     modules: {
-      localsConvention: 'camelCase',
-      generateScopedName: '[name]_[local]__[hash:base64:5]'
-    }
+      localsConvention: "camelCase",
+      generateScopedName: "[name]_[local]__[hash:base64:5]",
+    },
   },
   test: {
-    environment: 'jsdom',
-    setupFiles: ['./src/test/setup.js'],
+    environment: "jsdom",
+    setupFiles: ["./src/test/setup.js"],
     css: true,
     globals: true,
-    exclude: ['**/node_modules/**', '**/tests/**']
-  }
-})
+    exclude: ["**/node_modules/**", "**/tests/**"],
+  },
+});
